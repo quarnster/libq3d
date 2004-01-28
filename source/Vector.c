50e649479e0a7d10ea78ce4a7e0578e54f52e429
Fredrik Ehnbom <quarnster@gmail.com>
2004-01-28 20:14:47 +0000

    added Dot and Cross

--- source/Vector.c
+++ source/Vector.c
@@ -8,3 +8,16 @@ void q3dVectorNormalize(q3dTypeVector *v) {
 	v->z /= len;
 }
 
+float q3dVectorDot(q3dTypeVector *v1, q3dTypeVector *v2) {
+	return v1->x * v2->x + v1->y * v2->y + v1->z * v2->z;
+}
+
+void q3dVectorCross(q3dTypeVector *v1, q3dTypeVector *v2) {
+	float x = v1->y * v2->z - v1->z * v2->y;
+	float y = v1->z * v2->x - v1->x * v2->z;
+	float z = v1->x * v2->y - v1->y * v2->x;
+	v1->x = x;
+	v1->y = y;
+	v1->z = z;
+}
+
