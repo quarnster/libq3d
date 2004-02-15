4454aef90a893d5b756d1d4ef42a51fe36f5a0d5
Fredrik Ehnbom <quarnster@gmail.com>
2004-02-15 21:55:42 +0000

    added q3dColorSetV

--- include/Color.h
+++ include/Color.h
@@ -6,7 +6,7 @@
  * = Q3D - quarns quick and dirty 3D-engine ;) (c) Outbreak 2001-2002        =
  * ===========================================================================
  *
- * @id		:	$Id: Color.h,v 1.1 2004/01/24 21:41:43 quarn Exp $
+ * @id		:	$Id: Color.h,v 1.2 2004/02/15 21:55:42 quarn Exp $
  * @brief	:	Just a color structure
  * @author	:	Fredrik Ehnbom
  *
@@ -19,6 +19,7 @@ typedef struct {
 	float a;
 } q3dTypeColor;
 
+void q3dColorSetV(q3dTypeColor *dest, q3dTypeColor *source);
 void q3dColorSet3f(q3dTypeColor *color, float r, float g, float b);
 void q3dColorSet4f(q3dTypeColor *color, float r, float g, float b, float a);
 
--- source/Color.c
+++ source/Color.c
@@ -1,5 +1,12 @@
 #include "Color.h"
 
+void q3dColorSetV(q3dTypeColor *dest, q3dTypeColor *source) {
+	dest->a = source->a;
+	dest->r = source->r;
+	dest->g = source->g;
+	dest->b = source->b;
+}
+
 void q3dColorSet3f(q3dTypeColor *c, float r, float g, float b) {
 	c->r = r;
 	c->g = g;
