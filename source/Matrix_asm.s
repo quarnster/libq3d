b7b67a25b0d4e76fe7e4e4d91992ccc2b7b52e6e
Fredrik Ehnbom <quarnster@gmail.com>
2004-02-18 14:45:00 +0000

    made Transform into TransformPVR and added a regular transfrom-function

--- source/Matrix_asm.s
+++ source/Matrix_asm.s
@@ -1,5 +1,5 @@
 
-	.globl	_q3dMatrixTransformCopy,_q3dMatrixTransform
+	.globl	_q3dMatrixTransformCopy,_q3dMatrixTransform, _q3dMatrixTransformPVR
 
 	.text
 
@@ -37,6 +37,7 @@ _q3dMatrixTransformCopy:
 	! r7 = target pitch
 
 _q3dMatrixTransform:
+	pref	@r4
 	mov	r5,r0
 	mov	#4,r1
 	mov	#8,r2
@@ -44,6 +45,47 @@ _q3dMatrixTransform:
 	fmov.s	@r4+,fr0	! x
 	fmov.s	@r4+,fr1	! y
 	fmov.s	@r4+,fr2	! z
+	add	#4,r4		! skip the w-coordinate.. it's not used
+
+	fldi1	fr3		! w
+	ftrv	xmtrx,fv0
+
+	fdiv	fr3,fr0
+	fmov.s	fr0,@r0
+	fdiv	fr3,fr1
+	fmov.s	fr1,@(r0,r1)
+	fdiv	fr3,fr2
+	fmov.s	fr2,@(r0,r2)
+	add	r7,r0
+	dt	r6
+	bf	.loop
+
+	rts
+	nop
+
+	! Multiply a set of 3D vectors with the matrix
+	! (vectors are extended to 4D homogenous coordinates by
+	!  setting W=1), and then normalize the resulting
+	! vectors before storing them in the result array
+	! This function also makes the z-coordinates to be (1 / w) so
+	! correct sorting occur on the powwer vr-card in the dreamcast
+	!
+	! r4 = pointer to a source set of 3D vectors (n * 3 floats)
+	! r5 = pointer to a destination set of 3D vectors (n * 4 floats)
+	!      output vector is (x, y, z, -1/w)
+	! r6 = number of vectors to transform
+	! r7 = target pitch
+
+_q3dMatrixTransformPVR:
+	pref	@r4
+	mov	r5,r0
+	mov	#4,r1
+	mov	#8,r2
+.loop:
+	fmov.s	@r4+,fr0	! x
+	fmov.s	@r4+,fr1	! y
+	fmov.s	@r4+,fr2	! z
+	add	#4,r4		! skip the w-coordinate.. it's not used
 	fldi1	fr3		! w
 	ftrv	xmtrx,fv0
 
@@ -53,7 +95,7 @@ _q3dMatrixTransform:
 	fmov.s	fr1,@(r0,r1)
 	fldi1	fr1
 	fdiv	fr3,fr1
-	fmov.s	fr1,@(r0,r2)
+	fmov.s	fr1,@(r0,r2)	! move 1 / w to the z-position in output vector
 	add	r7,r0
 	dt	r6
 	bf	.loop
