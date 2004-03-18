be8a72cb28c1ffb587b56ea293cb76613726ca30
Fredrik Ehnbom <quarnster@gmail.com>
2004-03-18 09:53:58 +0000

    now uses direct rendering

--- source/FillerEnvironment.c
+++ source/FillerEnvironment.c
@@ -15,14 +15,14 @@ void q3dFillerEnvironmentInit(q3dTypeFillerEnvironment *type) {
 }
 
 void q3dFillerEnvironmentUpdate(q3dTypePolyhedron *poly) {
-	int i;
-	for (i = 0; i < poly->vertexLength; i++) {
+	int i  = poly->vertexLength;
+	while (i--) {
 		float shade =  (-poly->_vertexNormal[i].z-0.5f);
 
 		float a = poly->material.color.a;// + shade;
-		float r = poly->material.color.r + shade;
-		float g = poly->material.color.g + shade;
-		float b = poly->material.color.b + shade;
+		float r = poly->material.color.r/* + shade*/;
+		float g = poly->material.color.g/* + shade*/;
+		float b = poly->material.color.b/* + shade*/;
 
 		r = r > 1.0f ? 1.0f : r < 0.0f ? 0.0f : r;
 		g = g > 1.0f ? 1.0f : g < 0.0f ? 0.0f : g;
@@ -36,15 +36,38 @@ void q3dFillerEnvironmentUpdate(q3dTypePolyhedron *poly) {
 
 void q3dFillerEnvironmentDraw(q3dTypePolyhedron *poly) {
 	int i;
+
+	pvr_vertex_t *vert;
+	pvr_vertex_t *vert2;
+	pvr_dr_state_t state;
+	pvr_dr_init(state);
+
 	for (i = 0; i < poly->polygonLength; i++) {
+		int j;
 
 		int len = poly->polygon[i].vertexLength-1;
-		int j;
+
 		for (j = 0; j < len; j++) {
-			poly->_finalVertex[poly->polygon[i].vertex[j]].flags = PVR_CMD_VERTEX;
-			pvr_prim(&poly->_finalVertex[poly->polygon[i].vertex[j]], sizeof(pvr_vertex_t));
+			vert2 = &poly->_finalVertex[poly->polygon[i].vertex[j]];
+			vert = pvr_dr_target(state);
+			vert->flags = PVR_CMD_VERTEX;
+			vert->x = vert2->x;
+			vert->y = vert2->y;
+			vert->z = vert2->z;
+			vert->u = vert2->u;
+			vert->v = vert2->v;
+			vert->argb = vert2->argb;
+			pvr_dr_commit(vert);
 		}
-		poly->_finalVertex[poly->polygon[i].vertex[len]].flags = PVR_CMD_VERTEX_EOL;
-		pvr_prim(&poly->_finalVertex[poly->polygon[i].vertex[len]], sizeof(pvr_vertex_t));
+		vert2 = &poly->_finalVertex[poly->polygon[i].vertex[len]];
+		vert = pvr_dr_target(state);
+		vert->flags = PVR_CMD_VERTEX_EOL;
+		vert->x = vert2->x;
+		vert->y = vert2->y;
+		vert->z = vert2->z;
+		vert->u = vert2->u;
+		vert->v = vert2->v;
+		vert->argb = vert2->argb;
+		pvr_dr_commit(vert);
 	}
 }
